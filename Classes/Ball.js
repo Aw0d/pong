3368bd88a92288beeb762ee85c124301fa7df964
Aw0d <awod2004@yahoo.com>
2023-01-03 17:59:43 +0100

    Suppr console.log

--- Classes/Ball.js
+++ Classes/Ball.js
@@ -53,7 +53,6 @@ class Ball {
         if (this.x - this.radius < paddle1.x + paddle1.width && (this.y + this.radius > paddle1.y && this.y + this.radius < paddle1.y + paddle1.height)) { // Si la balle touche la paddle 1
             const ballPaddleDiff = round(this.y-this.radius - (paddle1.y + paddle1.height / 2));
             const angle = round(90 - (ballPaddleDiff / (paddle1.height / 2 + this.radius)) * (90 - (180 - this.maxAngle)));
-            console.log('angle: ' + angle + '°\nballPaddleDiff: ' + ballPaddleDiff);
             bounce(angle);
 
             this.x = paddle1.x + paddle1.width + this.radius; // On remet la balle sur la paddle
@@ -61,7 +60,6 @@ class Ball {
         } else if (this.x + this.radius > canvas.width - paddle2.width && (this.y + this.radius > paddle2.y && this.y + this.radius < paddle2.y + paddle2.height)) { // Si la balle touche la paddle 2
             const ballPaddleDiff = round(this.y-this.radius - (paddle2.y + paddle2.height / 2));
             const angle = round(90 + (ballPaddleDiff / (paddle2.height / 2 + this.radius)) * (90 - (180 - this.maxAngle)));
-            console.log('angle: ' + angle + '°\nballPaddleDiff: ' + ballPaddleDiff);
             bounce(angle);
 
             this.x = paddle2.x - this.radius; // On remet la balle sur la paddle
